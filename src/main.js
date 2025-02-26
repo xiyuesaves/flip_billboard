ca657d02e0a321e766fa2b4c326cf0b88f3e2e50
xiyuesaves <xiyuesaves@gmail.com>
2025-02-26 13:31:45 +0800

    fix 修复显示错误竖线的问题

--- src/main.js
+++ src/main.js
@@ -4,27 +4,30 @@ import pic2 from "../pic2.jpg";
 import pic3 from "../pic3.jpg";
 import anime from "animejs/lib/anime.es.js";
 
-window.addEventListener("resize", () => {});
-
-const style = document.createElement("style");
-
 // 分割数
 const splitNum = 48;
+const afterPic = document.querySelector(".after-pic");
+const beforePic = document.querySelector(".before-pic");
 
 let play = 0;
+let page = 0;
+let animation;
+const pics = [pic1, pic2, pic3];
+
+const style = document.createElement("style");
 
 // 更新css变量值
 style.innerHTML = `
 :root {
   --prism-length: ${splitNum};
-  --bgimg-1: url('${pic2}');
-  --bgimg-2: url('${pic1}');
+  --bgimg-1: url('${pic1}');
+  --bgimg-2: url('${pic2}');
   --bgimg-3: url('${pic3}');
 }
 `;
-
 document.head.appendChild(style);
 
+const prismList = [];
 const html = `<div class="triangular-prism-border">
         <div class="triangular-prism">
           <div class="face front"></div>
@@ -32,7 +35,6 @@ const html = `<div class="triangular-prism-border">
           <div class="face right"></div>
         </div>
       </div>`;
-const prismList = [];
 
 function initPrism() {
   const app = document.getElementById("app");
@@ -50,12 +52,15 @@ function initPrism() {
     app.appendChild(node);
   }
 }
-
 initPrism();
-let page = 0;
-let animation;
+
 function nextPage() {
   play = 1;
+  afterPic.style.setProperty("--after-pic", `url('${pics[(page + 1) % 3]}')`);
+  beforePic.style.setProperty("--before-pic", `url('${pics[page]}')`);
+
+  afterPic.style.setProperty("--after-pic-offset", `-100%`);
+  beforePic.style.setProperty("--before-pic-offset", `0%`);
   return new Promise((resolve) => {
     animation = anime({
       targets: prismList,
@@ -63,11 +68,20 @@ function nextPage() {
       duration: 1000,
       easing: "easeInOutQuad",
       update: function () {
-        prismList.forEach((item) => {
+        let beforeIndex = splitNum;
+        let afterIndex = 0;
+        prismList.forEach((item, index) => {
           const { faces, prism, process } = item;
+          if (beforeIndex === splitNum && process === 0) {
+            beforeIndex = index;
+          }
+          if (process === 100) {
+            afterIndex = index + 1;
+          }
           prism.style.transform = `translateZ(calc(var(--center-height) * -1)) rotateY(${
             120 * page + 120 * (process / 100)
           }deg)`;
+          prism.setAttribute("data-process", process);
           faces[page].style.setProperty(
             "--shadow",
             `linear-gradient(to right, transparent, rgba(0, 0, 0, ${0.8 * Math.sin((process / 100) * Math.PI)}))`
@@ -77,6 +91,8 @@ function nextPage() {
             `linear-gradient(to left, transparent, rgba(0, 0, 0, ${0.8 * Math.sin((process / 100) * Math.PI)}))`
           );
         });
+        afterPic.style.setProperty("--after-pic-offset", `${-100 + (afterIndex / splitNum) * 100}%`);
+        beforePic.style.setProperty("--before-pic-offset", `${(beforeIndex / splitNum) * 100}%`);
       },
       delay: anime.stagger(100),
       complete: () => {
@@ -104,8 +120,7 @@ document.addEventListener("keydown", (e) => {
 
 async function loop() {
   await nextPage();
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
   loop();
 }
-
 loop();
