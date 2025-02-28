183f8479605cb4711868a23476914869e48d73be
xiyuesaves <xiyuesaves@gmail.com>
2025-02-28 17:05:22 +0800

    删除无用参数

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import pic2 from "../pic2.jpg";
 import pic3 from "../pic3.jpg";
 import Wallpaper from "./Wallpaper";
 
-const wallpaper = new Wallpaper(document.querySelector("#app"), 48, [pic1, pic2, pic3], 0);
+const wallpaper = new Wallpaper(document.querySelector("#app"), 48, [pic1, pic2, pic3]);
 
 async function loop() {
   await wallpaper.nextPic();
