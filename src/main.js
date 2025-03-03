4aef363a4a65f9ac885cde2330ff24fe7ea9b57d
xiyuesaves <xiyuesaves@gmail.com>
2025-03-03 13:56:03 +0800

    增加参数

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import pic3 from "../pic3.svg";
 import Wallpaper from "./Wallpaper";
 
 const wallpaper = new Wallpaper(document.querySelector("#app"), 48, [pic1, pic2, pic3]);
+const CHANGE_TIME = 60;
+let changetime = CHANGE_TIME;
 
 async function loop() {
+  await new Promise((resolve) => setTimeout(resolve, changetime));
   await wallpaper.nextPic();
   loop();
 }
@@ -15,12 +18,19 @@ loop();
 window.wallpaperPropertyListener = {
   applyUserProperties: function (properties) {
     console.log(properties);
-    if (properties.yourproperty) {
-      // Do something with yourproperty
+    if (properties.changetime && parseInt(properties.changetime.value) !== changetime) {
+      if (isNaN(parseInt(properties.changetime.value))) {
+        changetime = CHANGE_TIME;
+      } else {
+        changetime = parseInt(properties.changetime.value);
+      }
     }
-    if (properties.anotherproperty) {
-      // Do something with anotherproperty
-    }
-    // Add more properties here
+  },
+  userDirectoryFilesAddedOrChanged: function (propertyName, changedFiles) {
+    console.log(propertyName, changedFiles);
+    wallpaper.pics = changedFiles.map(item => `file:///${item}`);
+  },
+  userDirectoryFilesRemoved: function (propertyName, removedFiles) {
+    console.log(propertyName, removedFiles);
   },
 };
