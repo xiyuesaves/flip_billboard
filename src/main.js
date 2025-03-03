66c08c01aae5a6e1640c0ce8f5450e1f95b58230
xiyuesaves <xiyuesaves@gmail.com>
2025-03-03 13:27:14 +0800

    默认图片改为svg

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./style.scss";
-import pic1 from "../pic1.jpg";
-import pic2 from "../pic2.jpg";
-import pic3 from "../pic3.jpg";
+import pic1 from "../pic1.svg";
+import pic2 from "../pic2.svg";
+import pic3 from "../pic3.svg";
 import Wallpaper from "./Wallpaper";
 
 const wallpaper = new Wallpaper(document.querySelector("#app"), 48, [pic1, pic2, pic3]);
